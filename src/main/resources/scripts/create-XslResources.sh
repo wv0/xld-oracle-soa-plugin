61726a5b0f2f7f1c1768428523456713eaba397a
jdewinne <joris.dewinne@gmail.com>
2015-02-06 13:14:08 +0100

    Added license

--- src/main/resources/scripts/create-XslResources.sh
+++ src/main/resources/scripts/create-XslResources.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+#
+# THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS
+# FOR A PARTICULAR PURPOSE. THIS CODE AND INFORMATION ARE NOT SUPPORTED BY XEBIALABS.
+#
+
 <#include "/dda/common/ftl/directives.ftl">
 <#assign userName = deployed.container.host.oracleUser>
 <#assign groupName = deployed.container.host.oracleGroup>
